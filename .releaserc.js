18bd8b9bf238b6807d63961ed47f73c2f0ec1e36
ulic75 <ulic75@gmail.com>
2022-04-28 09:21:20 -0600

    chore: add changelog generation on release (#3)

--- .releaserc.js
+++ .releaserc.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     [
       "@semantic-release/git",
       {
