d7ba2e241f42f8b41d090488a4ad1ceff50690f6
Luca Félix <luca.ziegler.felix@gmail.com>
2023-04-16 13:17:02 +0200

    bug: fix default state not rendering card correctly in battery

--- src/utils/get-default-config.ts
+++ src/utils/get-default-config.ts
@@ -15,7 +15,7 @@ export default function getDefaultConfig(hass: HomeAssistant): object {
   const gridPowerTestString = ["grid", "utility", "net", "meter"];
   const solarTests = ["solar", "pv", "photovoltaic", "inverter"];
   const batteryTests = ["battery"];
-  const batteryPercentTests = ["battery_percent", "battery_level", "state_of_charge", "soc"];
+  const batteryPercentTests = ["battery_percent", "battery_level", "state_of_charge", "soc", "percentage"];
   const firstGridPowerEntity = powerEntities.filter((entityId) => checkStrings(entityId, gridPowerTestString))[0];
   const firstSolarPowerEntity = powerEntities.filter((entityId) => checkStrings(entityId, solarTests))[0];
   const firstBatteryPowerEntity = powerEntities.filter((entityId) => checkStrings(entityId, batteryTests))[0];
@@ -29,9 +29,9 @@ export default function getDefaultConfig(hass: HomeAssistant): object {
   const firstBatteryPercentageEntity = percentageEntities.filter((entityId) => checkStrings(entityId, batteryPercentTests))[0];
   return {
     entities: {
-      battery: (firstBatteryPowerEntity && firstBatteryPercentageEntity) ?? {
-        entity: firstBatteryPowerEntity,
-        state_of_charge: firstBatteryPercentageEntity,
+      battery: {
+        entity: firstBatteryPowerEntity ?? "",
+        state_of_charge: firstBatteryPercentageEntity ?? "",
       },
       grid: firstGridPowerEntity ? { entity: firstGridPowerEntity } : undefined,
       solar: firstSolarPowerEntity ? { entity: firstSolarPowerEntity } : undefined,
