7685a8805e8c9d994f704abc3273b567b2da9a00
GHue <guido.huettemann@gmail.com>
2026-03-02 20:42:55 +0100

    feat(03-02): create gridHouseToHeatpump and gridMainToHeatpump flow files
    - flowGridHouseToHeatpump: curved SVG monodirectional from grid_house to heatpump
    - flowGridMainToHeatpump: curved SVG monodirectional from grid_main to heatpump
    - Both use showLine/styleLine/checkShouldShowDots utilities
    - Both use curved SVG path (no class=flat-line)
    - Duration from newDur.heatpumpFromGridHouse / newDur.heatpumpFromGridMain

---
+++ src/components/flows/gridHouseToHeatpump.ts
@@ -0,0 +1,47 @@
+import { classMap } from "lit/directives/class-map.js";
+import { PowerFlowCardPlusConfig } from "@/power-flow-card-plus-config";
+import { showLine } from "@/utils/showLine";
+import { html, svg } from "lit";
+import { styleLine } from "@/utils/styleLine";
+import { type Flows } from "./index";
+import { checkHasBottomIndividual, checkHasRightIndividual } from "@/utils/computeIndividualPosition";
+import { checkShouldShowDots } from "@/utils/checkShouldShowDots";
+
+type FlowsWithHeatpump = Flows & { heatpump: any };
+
+export const flowGridHouseToHeatpump = (config: PowerFlowCardPlusConfig, { battery, grid, heatpump, individual, newDur }: FlowsWithHeatpump) => {
+  return heatpump.has && grid.has && showLine(config, heatpump.flowFromGridHouse)
+    ? html`<div
+        class="lines ${classMap({
+          high: battery.has || checkHasBottomIndividual(individual),
+          "individual1-individual2": !battery.has && individual.every((i: any) => i?.has),
+          "multi-individual": checkHasRightIndividual(individual),
+        })}"
+        id="grid-house-heatpump-flow"
+      >
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice">
+          <path
+            id="grid-house-heatpump"
+            class="heatpump ${styleLine(heatpump.flowFromGridHouse || 0, config)}"
+            d="M80,50 v15 c0,20 -20,20 -35,20 h-5"
+            vector-effect="non-scaling-stroke"
+          ></path>
+          ${checkShouldShowDots(config) && heatpump.flowFromGridHouse
+            ? svg`<circle
+            r="1"
+            class="heatpump"
+            vector-effect="non-scaling-stroke"
+          >
+            <animateMotion
+              dur="${newDur.heatpumpFromGridHouse}s"
+              repeatCount="indefinite"
+              calcMode="linear"
+            >
+              <mpath xlink:href="#grid-house-heatpump" />
+            </animateMotion>
+          </circle>`
+            : ""}
+        </svg>
+      </div>`
+    : "";
+};
---
+++ src/components/flows/gridMainToHeatpump.ts
@@ -0,0 +1,47 @@
+import { classMap } from "lit/directives/class-map.js";
+import { PowerFlowCardPlusConfig } from "@/power-flow-card-plus-config";
+import { showLine } from "@/utils/showLine";
+import { html, svg } from "lit";
+import { styleLine } from "@/utils/styleLine";
+import { type Flows } from "./index";
+import { checkHasBottomIndividual, checkHasRightIndividual } from "@/utils/computeIndividualPosition";
+import { checkShouldShowDots } from "@/utils/checkShouldShowDots";
+
+type FlowsWithHeatpump = Flows & { heatpump: any; gridMain?: any };
+
+export const flowGridMainToHeatpump = (config: PowerFlowCardPlusConfig, { battery, gridMain, heatpump, individual, newDur }: FlowsWithHeatpump) => {
+  return heatpump.has && gridMain?.has && showLine(config, heatpump.flowFromGridMain)
+    ? html`<div
+        class="lines ${classMap({
+          high: battery.has || checkHasBottomIndividual(individual),
+          "individual1-individual2": !battery.has && individual.every((i: any) => i?.has),
+          "multi-individual": checkHasRightIndividual(individual),
+        })}"
+        id="grid-main-heatpump-flow"
+      >
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice">
+          <path
+            id="grid-main-heatpump"
+            class="heatpump ${styleLine(heatpump.flowFromGridMain || 0, config)}"
+            d="M20,50 v15 c0,20 20,20 35,20 h5"
+            vector-effect="non-scaling-stroke"
+          ></path>
+          ${checkShouldShowDots(config) && heatpump.flowFromGridMain
+            ? svg`<circle
+            r="1"
+            class="heatpump"
+            vector-effect="non-scaling-stroke"
+          >
+            <animateMotion
+              dur="${newDur.heatpumpFromGridMain}s"
+              repeatCount="indefinite"
+              calcMode="linear"
+            >
+              <mpath xlink:href="#grid-main-heatpump" />
+            </animateMotion>
+          </circle>`
+            : ""}
+        </svg>
+      </div>`
+    : "";
+};
